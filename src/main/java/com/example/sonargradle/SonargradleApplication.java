0c3d757bede48dcee22db41b1fdbd91b914d80d5
Robério Gomes Patricio <roberiogomes@gmail.com>
2022-01-03 10:57:18 -0300

    Initial files

---
+++ src/main/java/com/example/sonargradle/SonargradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonargradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonargradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonargradleApplication.class, args);
+	}
+
+}
